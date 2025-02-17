e0862eae2e99d2bb07a2ca0eb8407e60593025a2
aalssadah <ala.alssadah@gmail.com>
2025-02-17 11:09:10 +0300

    try tailwind importation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="text-4xl font-bold text-blue-500 text-center">
            title
         </h2>
     </x-slot>

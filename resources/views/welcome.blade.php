90fc4c0a3e39ea3ff985dcc88cf4b380cf1113ee
aalssadah <ala.alssadah@gmail.com>
2025-02-18 11:25:09 +0300

    flex and grid

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,6 @@
 <x-app-layout>
     
-    <div class=" py-12 px-8 ">
-        
+    <div class=" py-12 px-8 max-w-md mx-auto">
         
         <img src="/images/logo.png "  alt="image" 
         class=" h-24"/>
@@ -9,7 +8,7 @@
         <img src="/images/bg.jpg" alt="background"
         class=" mt-6 rounded-xl shadow-xl h-64 "/>
 
-        <h1 class=" text-2xl font-medium text-gray-700 leading-tight">
+        <h1 class=" text-2xl font-medium text-gray-700 leading-tight mt-4">
             You can work from anywhere. 
             <span class=" text-green-700 font-bold">Take advantage of it.</span>
         </h1>
@@ -19,10 +18,26 @@
         </p>
 
         <div class="mt-4">
-            <a href="#" class=" bg-green-700 text-sm text-white px-5 py-3 rounded-lg shadow-lg uppercase tracking-wider font-semibold">
+            <a href="#" class=" bg-green-700 inline-block text-sm text-white px-5 py-3 rounded-lg shadow-lg uppercase tracking-wider font-semibold">
             Book your escape    
             </a>
         </div>
+        
+        <div class="grid grid-cols-2 gap-3">
+            <div style="background: purple; padding: 10px;">Box 1</div>
+            <div style="background: orange; padding: 10px;">Box 2</div>
+            <div style="background: orange; padding: 10px;">Box 3</div>
+            <div class="flex gap-4">
+                <div class="bg-red-300 p-4">Item 1</div>
+                <div class="bg-blue-300 p-4">Item 2</div>
+                <div class="bg-green-300 p-4">Item 3</div>  
+            </div>
+        </div>
+
+        
+        
+        
+        
 
     </div>
         

fa49435ac6ee5f6c52c4bc087e68648c812d848f
aalssadah <ala.alssadah@gmail.com>
2025-02-18 13:48:23 +0300

    add responsive

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,44 +1,34 @@
 <x-app-layout>
     
-    <div class=" py-12 px-8 max-w-md mx-auto">
+    <div class="flex bg-gray-100">
+        
+    <div class=" py-12 px-8 max-w-md mx-auto sm:max-w-xl lg:max-w-full lg:w-1/2">
         
         <img src="/images/logo.png "  alt="image" 
         class=" h-24"/>
 
         <img src="/images/bg.jpg" alt="background"
-        class=" mt-6 rounded-xl shadow-xl h-64 "/>
+        class=" mt-6 rounded-xl shadow-xl  sm:mt-8 sm:h-64 sm:w-full object-cover object-center lg:hidden"/>
 
-        <h1 class=" text-2xl font-medium text-gray-700 leading-tight mt-4">
+        <h1 class=" text-2xl sm:text-4xl font-medium text-gray-700 leading-tight mt-4 sm:mt-8">
             You can work from anywhere. 
             <span class=" text-green-700 font-bold">Take advantage of it.</span>
         </h1>
 
-        <p class=" text-gray-600 mt-2">
+        <p class=" text-gray-600 mt-2 sm:text-xl sm:mt-4">
             Workcation helps you find work-friendly rentals in beautiful locations so you can enjoy some nice weather even when you're not on vacation.
         </p>
 
-        <div class="mt-4">
-            <a href="#" class=" bg-green-700 inline-block text-sm text-white px-5 py-3 rounded-lg shadow-lg uppercase tracking-wider font-semibold">
+        <div class="mt-4 ">
+            <a href="#" class=" bg-green-700 inline-block text-sm text-white px-5 py-3 rounded-lg shadow-lg uppercase tracking-wider font-semibold
+            sm:text-base">
             Book your escape    
             </a>
         </div>
-        
-        <div class="grid grid-cols-2 gap-3">
-            <div style="background: purple; padding: 10px;">Box 1</div>
-            <div style="background: orange; padding: 10px;">Box 2</div>
-            <div style="background: orange; padding: 10px;">Box 3</div>
-            <div class="flex gap-4">
-                <div class="bg-red-300 p-4">Item 1</div>
-                <div class="bg-blue-300 p-4">Item 2</div>
-                <div class="bg-green-300 p-4">Item 3</div>  
-            </div>
-        </div>
-
-        
-        
-        
-        
-
     </div>
-        
+
+    <div class="hidden lg:block lg:w-1/2 lg:relative">
+        <img src="/images/bg.jpg" alt="background" class=" absolute inset-0 w-full h-full object-cover object-center"/>
+    </div>
+</div>
 </x-app-layout>

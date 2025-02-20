2942f35fcbe8fb64f25d8cdd917edf6758396c98
aalssadah <ala.alssadah@gmail.com>
2025-02-20 13:33:08 +0300

    second card

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -59,4 +59,28 @@
 </div>
 </section>
 
+
+<section class="wrapper">
+    
+        <div class="grid grid-cols-1 md:grid-cols-3 bg-white rounded-lg">
+                <img src="/images/card.jpg" class=" h-full rounded-l-lg col-span-1  object-cover object-center">
+
+                <div class="col-span-2 p-6 flex flex-col">
+                    <h1 class=" font-normal text-2xl mt-2 ">Noteworthy technology acquisitions 2021</h1>
+                    <p class=" mb-3 text-md opacity-20 mt-2">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    <a href="#" class=" w-32 mt-auto inline-flex items-center justify-center bg-green-700  text-white text-sm font-medium  px-3 py-2 rounded-lg ">
+                            Read more
+                            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                            </svg>
+                    </a>
+                </div>
+
+        </div>
+
+        
+    
+    
+</section>
+
 </x-app-layout>

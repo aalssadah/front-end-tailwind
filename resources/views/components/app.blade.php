b22b6afbd13080a254b435ff6a58e1ddee291308
aalssadah <ala.alssadah@gmail.com>
2025-02-21 18:02:43 +0300

    fix dot before title

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -2,20 +2,23 @@
 
     <div class="bg-[#E2F5F4] grid grid-cols-1 sm:grid-cols-3 rounded-2xl h-full relative">
 
+    
 
+        
         <div class="justify-between col-span-2 flex flex-col p-6 gap-6 max-w-xl ">
-            <div class="">
-
-                <img class="" src="/imag"> 
-                <h1 class=" text-2xl opacity-60 font-semibold"> Download Naway App</h1>
+            <div class="text">
+                <div class="inline-flex justify-center items-center">
+                    <img class=" mr-2" src="/images/dot.svg">
+                    <h1 class=" text-2xl opacity-60 font-semibold"> Download Naway App</h1>
+                </div>
                 <p class=" text-l mt-4">We approach every project with the same passion, enthusiasm and attention
                     to detail that leads us to achieve your project goals with ease.</p>
-                </div>
-                <div class="stores flex flex-wrap gap-3">
-                    <img class=" w-40" src="/images/appStore.png">
-                    <img class=" w-40" src="/images/playStore.png">
-                    <img class=" w-40" src="/images/huawei.webp">
-                </div>
+            </div>
+            <div class="stores flex flex-wrap gap-3">
+                <img class=" w-40" src="/images/appStore.png">
+                <img class=" w-40" src="/images/playStore.png">
+                <img class=" w-40" src="/images/huawei.webp">
+            </div>
         </div>
 
         <div class=" h-[400px] w-[250px] overflow-hidden col-span-1 mt-[-80px] hidden md:block">

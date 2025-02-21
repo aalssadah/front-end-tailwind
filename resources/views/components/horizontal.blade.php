629b83cc7c707f3feba832326e5919161205982f
aalssadah <ala.alssadah@gmail.com>
2025-02-21 15:39:07 +0300

    add discount

---
+++ resources/views/components/horizontal.blade.php
@@ -0,0 +1,34 @@
+
+    
+        <div class="grid grid-cols-1 md:grid-cols-3 bg-white rounded-lg mt-6 relative">
+            <div class=" inline-flex md:hidden absolute top-0 left-0  bg-yellow-200 p-2 rounded-tl-lg">
+                <h1 class="text-sm text-gray-800"> Discount 30%</h1>
+                <svg class="w-3 mx-1  items-center justify-center" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 80L0 229.5c0 17 6.7 33.3 18.7 45.3l176 176c25 25 65.5 25 90.5 0L418.7 317.3c25-25 25-65.5 0-90.5l-176-176c-12-12-28.3-18.7-45.3-18.7L48 32C21.5 32 0 53.5 0 80zm112 32a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>
+            </div>
+                <img src="/images/street.jpg" class=" h-full rounded-l-lg col-span-1  object-cover object-center">
+
+                <div class="col-span-2 p-6 flex flex-col ">
+                    <div class=" hidden md:inline-flex absolute top-0 right-0 bg-yellow-200  p-2 rounded-tr-lg ">
+                        
+                        <h1 class="  text-sm text-gray-800"> Discount 30%</h1>
+                        <svg class="w-3 mx-1  items-center justify-center" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 80L0 229.5c0 17 6.7 33.3 18.7 45.3l176 176c25 25 65.5 25 90.5 0L418.7 317.3c25-25 25-65.5 0-90.5l-176-176c-12-12-28.3-18.7-45.3-18.7L48 32C21.5 32 0 53.5 0 80zm112 32a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>
+                        {{-- <svg class=" w-3 mx-1  items-center justify-center" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M374.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-320 320c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l320-320zM128 128A64 64 0 1 0 0 128a64 64 0 1 0 128 0zM384 384a64 64 0 1 0 -128 0 64 64 0 1 0 128 0z"/></svg> --}}
+                    </div>
+                    <h1 class=" font-normal text-2xl mt-2 ">Noteworthy technology acquisitions 2021</h1>
+                    <p class=" mb-3 text-md opacity-20 mt-2">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    <a href="#" class=" w-32 mt-auto inline-flex items-center justify-center bg-green-700  text-white text-sm font-medium  px-3 py-2 rounded-lg ">
+                            Read more
+                            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                            </svg>
+                    </a>
+                </div>
+
+        </div>
+
+        
+
+        
+    
+    
+

bcc2fcf78b768ce93ca2280162743f5c67479913
aalssadah <ala.alssadah@gmail.com>
2025-02-23 11:16:23 +0300

    add dynamic stars to card

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -7,14 +7,17 @@
             <h4 class=" font-semibold text-xl leading-tight truncate">This is the card you asked for
                 This is the card you asked for
             </h4>
-            <div class="mt-1"> $1900 
-            <span class=" text-gray-600 text-sm">/ wk </span>
+            <div class="mt-1"> $1900
+                <span class=" text-gray-600 text-sm">/ wk </span>
             </div>
-            <div>
-                 <span class=" text-teal-500 font-semibold">4/5 starts</span>
-                 <span class=" text-gray-500 text-sm">  (based on 34 reviews) </span>
-                </div>
-            
+            <div class="flex mt-4">
+                @for ($i = 1; $i <= 5; $i++)
+                    <x-star width="4" height="4" color="{{$i<=3?'text-teal-500':'text-gray-400'}}">
+                    </x-star>
+                @endfor
+                <span class=" text-gray-500 text-sm ml-3"> 34 reviews </span>
+            </div>
+
         </div>
 
     </div>

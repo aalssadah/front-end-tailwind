f81a5ecf6d4869ed279f9f178a4242a293341d29
aalssadah <ala.alssadah@gmail.com>
2025-02-23 10:22:44 +0300

    add cart for mobile

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,10 +1,19 @@
 <section class="wrapper">
 
-    <div class="bg-white ">
+    <div class="bg-white rounded-lg overflow-hidden text-gray-900">
         <img src="/images/card.webp" alt="">
         <div class="p-6">
-            <h1>This is the card you asked for</h1>
-            <h4> Allowed in Negira</h4>
+            <div class="text-xs font-semibold text-gray-600 uppercase tracking-wide"> 3 beds &bull; 2 baths</div>
+            <h4 class=" font-semibold text-xl leading-tight truncate">This is the card you asked for
+                This is the card you asked for
+            </h4>
+            <div class="mt-1"> $1900 
+            <span class=" text-gray-600 text-sm">/ wk </span>
+            </div>
+            <div>
+                 <span class=" text-teal-500 font-semibold">4/5 starts</span>
+                 <span class=" text-gray-500 text-sm">  (based on 34 reviews) </span>
+                </div>
             
         </div>
 

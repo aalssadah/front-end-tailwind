736ae743ea9dfd02763b99147b7ca43d77b372aa
ala <ala.alssadah@gmail.com>
2025-03-02 22:53:21 +0300

    add hero slider

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,27 @@
 <x-app-layout>
 
+<section class="w-full h-[588px] relative  overflow-hidden bg-main">
+    <div class="video  w-full h-full ">
+        <video class=" absolute z-0 min-w-full min-h-full opacity-25 "  autoplay loop muted playsinline>
+            <source src="/hero-bg.mp4" class="w-full h-full" type="video/mp4">
+        </video>
+        <div class=" md:max-w-5xl mx-auto items-center gap-8 pt-12 pb-32 px-4 flex flex-col h-full ">
+            <h1 class="text-2xl text-center text-white">
+        حلولك الرقمية تبدأ من
+        <span class="text-teal-500">هنا</span>
+            </h1>
+            <p class="text-xl text-white text-center ">
+                احصل على خدمات رقمية احترافية ومتكاملة من أول تطوير التطبيقات و المواقع والمتاجر الإلكترونية مروراً بالتصاميم الجرافيكية بأنواعها وحتى باقات متنوعة من خدمات التسويق الإلكتروني مقدمة من فريقنا المتميز.
+            </p>
 
-    
+            <div class=" text-white inline-flex gap-4">
+                <a href="#" class="px-8 py-4 rounded-full bg-transparent  border border-teal-300" type="submit">البروفايل التعريفي</a>
+                <a href="#" class="px-8 py-4 rounded-full bg-[#1CD2E2FF] " type="submit">اطلب خدمة</a>
+            </div>
+    </div>
+    </div>
+
+</section>
+
 
 </x-app-layout>

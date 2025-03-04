ef4109c16a6105dd2bb4e474ec2029f393b309c5
aalssadah <ala.alssadah@gmail.com>
2025-03-04 13:34:00 +0300

    flex to service component

--- resources/views/components/home/service.blade.php
+++ resources/views/components/home/service.blade.php
@@ -1,4 +1,22 @@
-<section class="  h-72 bg-orange-400">
-    
+<section class="  h-72 py-16">
+
+    <div class="flex flex-col items-center gap-5">
+
+        <div class="">
+            <h1 class="text-2xl font-semibold">
+                خدماتنا
+            </h1>
+        </div>
+
+        <div class="">
+            <img src="/images/dot.svg" alt="" class="w-4 h-4">
+        </div>
+
+        <div class="">
+            <p class=" text-xl font-medium  opacity-50">
+                نتعامل مع كل مشروع بذات الشغف والحماس والاهتمام بالتفاصيل الذي يقودنا لتحقيق أهداف مشروعك بسهولة
+            </p>
+        </div>
+    </div>
 
 </section>

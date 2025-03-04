ba5209aeeea66225e89f8a2c3dce12b048de9e6e
aalssadah <ala.alssadah@gmail.com>
2025-03-04 14:38:28 +0300

    add service cards

--- resources/views/components/home/service.blade.php
+++ resources/views/components/home/service.blade.php
@@ -19,4 +19,68 @@
         </div>
     </div>
 
+    <div class="service-card grid grid-cols-2 gap-4 justify-center">
+
+        <div class="flex  gap-4 px-8 py-12 border  rounded-2xl shadow-lg bg-[#FAFAFA] hover:bg-[#00f5f538] duration-700 transition-all">
+            <div class="flex flex-col justify-between gap-6">
+                <div class="">
+                    <h1 class="text-xl font-semibold">
+                        برمجة المواقع الإلكترونية
+                    </h1>
+                </div>
+
+                <div class="">
+
+                    <p class="text-l font-medium opacity-60 mt-4">
+                        نقدم خدمات برمجة وتطوير المواقع الإلكترونية بتصاميم مميزة وتجربة مستخدم متطورة، مع ضمان توافقها مع جميع الأجهزة وسهولة التصفح. سواء كنت بحاجة إلى موقع تعريفي، مدونة، أو منصة إلكترونية، فنحن نقدم لك الحلول المثالية.
+                    </p>
+                </div>
+
+
+                <div class="">
+
+                    <a href="#" type="submit" class=" px-3 py-1  rounded-full text-[#06C4BF] border border-[#06C4BF]">
+                            عرض المزيد
+                    </a>
+                </div>
+            </div>
+
+            <div class=" w-full">
+                <img src="/images/service.png">
+            </div>
+
+        </div>
+
+        <div class="flex  gap-4 px-8 py-12 border  rounded-2xl shadow-lg bg-[#FAFAFA] hover:bg-[#00f5f538] duration-700 transition-all">
+            <div class="flex flex-col justify-between gap-6">
+                <div class="">
+                    <h1 class="text-xl font-semibold">
+                        برمجة المواقع الإلكترونية
+                    </h1>
+                </div>
+
+                <div class="">
+
+                    <p class="text-l font-medium opacity-60 mt-4">
+                        نقدم خدمات برمجة وتطوير المواقع الإلكترونية بتصاميم مميزة وتجربة مستخدم متطورة، مع ضمان توافقها مع جميع الأجهزة وسهولة التصفح. سواء كنت بحاجة إلى موقع تعريفي، مدونة، أو منصة إلكترونية، فنحن نقدم لك الحلول المثالية.
+                    </p>
+                </div>
+
+
+                <div class="">
+
+                    <a href="#" type="submit" class=" px-3 py-1  rounded-full text-[#06C4BF] border border-[#06C4BF]">
+                            عرض المزيد
+                    </a>
+                </div>
+            </div>
+
+            <div class=" w-full">
+                <img src="/images/service.png">
+            </div>
+
+        </div>
+
+    </div>
+
 </section>

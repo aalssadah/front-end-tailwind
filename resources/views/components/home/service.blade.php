e92d2fca658ef6d83b8e4402667ab19646fc141f
aalssadah <ala.alssadah@gmail.com>
2025-03-06 12:31:24 +0300

    edit service

--- resources/views/components/home/service.blade.php
+++ resources/views/components/home/service.blade.php
@@ -19,14 +19,15 @@
         </div>
     </div>
 
-    <section class=" max-w-7xl mx-auto px-12 py-24 hidden sm:block">
+    {{-- web card --}}
+    <section class=" max-w-7xl mx-auto px-12 py-24 hidden md:block">
 
         <div class="service-card grid md:grid-cols-2 grid-cols-1 gap-4 justify-center ">
 
             @for ($i = 0; $i < 5; $i++)
-                <div
-                    class="flex  gap-4 px-8 py-12 border  rounded-2xl shadow-lg bg-[#FAFAFA] hover:bg-[#00f5f517] duration-700 transition-all">
+                <div class="flex  gap-4 px-8 py-12 border  rounded-2xl shadow-lg bg-[#FAFAFA] hover:bg-[#00f5f517] duration-700 transition-all">
                     <div class="flex flex-col justify-between gap-6">
+
                         <div class="">
                             <h1 class="text-xl font-semibold">
                                 برمجة المواقع الإلكترونية
@@ -37,8 +38,7 @@
 
                             <p class="text-l font-medium opacity-60 mt-4">
                                 نقدم خدمات برمجة وتطوير المواقع الإلكترونية بتصاميم مميزة وتجربة مستخدم متطورة، مع ضمان
-                                توافقها مع جميع الأجهزة وسهولة التصفح. سواء كنت بحاجة إلى موقع تعريفي، مدونة، أو منصة
-                                إلكترونية، فنحن نقدم لك الحلول المثالية.
+                                توافقه
                             </p>
                         </div>
 
@@ -52,16 +52,18 @@
                         </div>
                     </div>
 
-                    <div class=" w-full">
+                    <div class=" w-full h-full">
                         <img src="/images/service.png">
                     </div>
 
                 </div>
             @endfor
-
-
-
         </div>
+    </section>
+
+    {{-- Mobile card --}}
+    <section class="max-w-7xl mx-auto py-24 block md:hidden">
+
     </section>
 
 

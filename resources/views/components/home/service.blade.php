6ad75d650e3b45aaf27e0ca643592d2f1c318373
aalssadah <ala.alssadah@gmail.com>
2025-03-06 11:46:08 +0300

    responsive service card

--- resources/views/components/home/service.blade.php
+++ resources/views/components/home/service.blade.php
@@ -1,4 +1,4 @@
-<section class="  h-72 py-16">
+<section class="  h-72 py-16 ">
 
     <div class="flex flex-col items-center gap-5">
 
@@ -12,53 +12,57 @@
             <img src="/images/dot.svg" alt="" class="w-4 h-4">
         </div>
 
-        <div class="">
+        <div class="text-center">
             <p class=" text-xl font-medium  opacity-50">
                 نتعامل مع كل مشروع بذات الشغف والحماس والاهتمام بالتفاصيل الذي يقودنا لتحقيق أهداف مشروعك بسهولة
             </p>
         </div>
     </div>
 
-    <section class=" max-w-7xl mx-auto px-12 py-24">
+    <section class=" max-w-7xl mx-auto px-12 py-24 hidden sm:block">
 
-    <div class="service-card grid grid-cols-2 gap-4 justify-center ">
+        <div class="service-card grid md:grid-cols-2 grid-cols-1 gap-4 justify-center ">
 
-        @for($i=0;$i<5;$i++)
-        <div class="flex  gap-4 px-8 py-12 border  rounded-2xl shadow-lg bg-[#FAFAFA] hover:bg-[#00f5f538] duration-700 transition-all">
-            <div class="flex flex-col justify-between gap-6">
-                <div class="">
-                    <h1 class="text-xl font-semibold">
-                        برمجة المواقع الإلكترونية
-                    </h1>
+            @for ($i = 0; $i < 5; $i++)
+                <div
+                    class="flex  gap-4 px-8 py-12 border  rounded-2xl shadow-lg bg-[#FAFAFA] hover:bg-[#00f5f517] duration-700 transition-all">
+                    <div class="flex flex-col justify-between gap-6">
+                        <div class="">
+                            <h1 class="text-xl font-semibold">
+                                برمجة المواقع الإلكترونية
+                            </h1>
+                        </div>
+
+                        <div class="">
+
+                            <p class="text-l font-medium opacity-60 mt-4">
+                                نقدم خدمات برمجة وتطوير المواقع الإلكترونية بتصاميم مميزة وتجربة مستخدم متطورة، مع ضمان
+                                توافقها مع جميع الأجهزة وسهولة التصفح. سواء كنت بحاجة إلى موقع تعريفي، مدونة، أو منصة
+                                إلكترونية، فنحن نقدم لك الحلول المثالية.
+                            </p>
+                        </div>
+
+
+                        <div class="">
+
+                            <a href="#" type="submit"
+                                class=" px-3 py-1  rounded-full text-[#06C4BF] border border-[#06C4BF]">
+                                عرض المزيد
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class=" w-full">
+                        <img src="/images/service.png">
+                    </div>
+
                 </div>
-
-                <div class="">
-
-                    <p class="text-l font-medium opacity-60 mt-4">
-                        نقدم خدمات برمجة وتطوير المواقع الإلكترونية بتصاميم مميزة وتجربة مستخدم متطورة، مع ضمان توافقها مع جميع الأجهزة وسهولة التصفح. سواء كنت بحاجة إلى موقع تعريفي، مدونة، أو منصة إلكترونية، فنحن نقدم لك الحلول المثالية.
-                    </p>
-                </div>
+            @endfor
 
 
-                <div class="">
-
-                    <a href="#" type="submit" class=" px-3 py-1  rounded-full text-[#06C4BF] border border-[#06C4BF]">
-                            عرض المزيد
-                    </a>
-                </div>
-            </div>
-
-            <div class=" w-full">
-                <img src="/images/service.png">
-            </div>
 
         </div>
+    </section>
 
-        @endfor
-        
-        
-
-    </div>
-</section>
 
 </section>

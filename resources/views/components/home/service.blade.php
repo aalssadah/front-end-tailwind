ed321c9c1005b1c7a11d56dc4c64fcf96d6b627d
ala <ala.alssadah@gmail.com>
2025-03-08 18:13:24 +0300

    fix centered the last item in service

--- resources/views/components/home/service.blade.php
+++ resources/views/components/home/service.blade.php
@@ -20,46 +20,48 @@
     </div>
 
     {{-- web card --}}
-    <section class=" max-w-7xl mx-auto px-2 md:px-8 lg:px-12 py-24 ">
+    <section class="max-w-7xl mx-auto px-2 md:px-8 lg:px-12 py-24">
 
-        <div class="service-card grid md:grid-cols-2 grid-cols-1 gap-4 justify-center ">
+        <div class="service-card grid md:grid-cols-2 gap-4">
+            @php
+                $totalItems = 9; // Change this dynamically based on your actual data
+            @endphp
 
-            @for ($i = 0; $i < 5; $i++)
-                <div class="flex  gap-4 px-8 py-12 border  rounded-2xl shadow-lg bg-[#FAFAFA] hover:bg-[#00f5f517] duration-700 transition-all">
-                    
+            @for ($i = 0; $i < $totalItems; $i++)
+                <div class="flex gap-4 px-8 py-12 border rounded-2xl shadow-lg bg-[#FAFAFA] hover:bg-[#00f5f517] duration-700 transition-all
+                        {{ $i == $totalItems - 1 && $totalItems % 2 != 0 ? 'md:col-span-2 mx-auto' : '' }}">
+
                     <div class="flex flex-col justify-between gap-6 w-3/4">
-
-                        <div class="">
+                        <div>
                             <h1 class="text-xl font-semibold">
                                 برمجة المواقع الإلكترونية
                             </h1>
                         </div>
 
-                        <div class="">
-
+                        <div>
                             <p class="text-l font-medium opacity-60 mt-4">
                                 نقدم خدمات برمجة وتطوير المواقع الإلكترونية بتصاميم مميزة وتجربة مستخدم متطورة
                             </p>
                         </div>
 
-
-                        <div class="">
-
-                            <a href="#" type="submit"
-                                class=" px-3 py-1  rounded-full text-[#06C4BF] border border-[#06C4BF]">
+                        <div>
+                            <a href="#" class="px-3 py-1 rounded-full text-[#06C4BF] border border-[#06C4BF]">
                                 عرض المزيد
                             </a>
                         </div>
                     </div>
 
-                    <div class=" w-1/3">
-                        <img src="/images/service.png" class=" h-[150px]">
+                    <div class="w-1/3">
+                        <img src="/images/service.png" class="h-[150px]">
                     </div>
 
                 </div>
             @endfor
         </div>
+
     </section>
+
+
 
     {{-- Mobile card --}}
     <section class="max-w-7xl mx-auto py-24 block md:hidden">

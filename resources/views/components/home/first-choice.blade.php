2fcf6c8a73177ffaa47f498d05304608121dd50b
aalssadah <ala.alssadah@gmail.com>
2025-03-10 13:58:36 +0300

    work on boxes

--- resources/views/components/home/first-choice.blade.php
+++ resources/views/components/home/first-choice.blade.php
@@ -1,11 +1,12 @@
-<section class="bg-cover bg-center pt-16  relative" style="background-image: url('{{ asset('images/firstchoice.png') }}');">
+<section class="bg-cover bg-center pt-16  relative"
+    style="background-image: url('{{ asset('images/firstchoice.png') }}');">
 
     {{-- <div class=" max-w-6xl mx-auto "> --}}
-<div class=" absolute inset-0 backdrop-blur-sm"></div>
-        <div class=" flex flex-col gap-4 text-center">
-            
-        <div class=" texts flex flex-col gap-2" style="    transform: translateZ(0px);opacity: 1">
-            <div class="font-semibold text-xl " >
+    <div class=" absolute inset-0 backdrop-blur-sm" style="background-color: rgba(13, 36, 73, 0.6);"></div>
+     <div class=" flex flex-col gap-8 text-center" style="    transform: translateZ(0px);opacity: 1">
+
+        <div class=" texts flex flex-col gap-2">
+            <div class="font-semibold text-xl ">
                 <h1 class=" text-white">مايجعلنا خيارك
                     <span class=" text-teal-300 ">الأول</span>
                 </h1>
@@ -14,24 +15,69 @@
             <div class=" mx-auto">
                 <img src="/images/dot.svg" class=" " alt="">
             </div>
-            
+
             <div class="">
                 <p class=" font-medium text-l text-white">
                     نحن نقدم حلولاً إلكترونية مبتكرة تتكامل بين التسويق الفعّال وبرمجة المواقع والتطبيقات بأعلى جودة
                     واهتمام بالتفاصيل
                 </p>
             </div>
-            
-        </div>
-
-        <div class="boxes">
 
         </div>
-        
-        
+
+        <div class="boxes max-w-6xl mx-auto px-8">
+            <div class="flex  gap-3">
+                <div class=" border border-opacity-5 border-white  bg-white/10 rounded-lg ">
+                    <div class="flex flex-col gap-3 items-center">
+                        <div class="">
+                            <img src="/images/star.svg" alt="">
+                        </div>
+                        <div class="">
+                            <h1 class="text-white text-xl">سرعة التنفيذ</h1>
+                        </div>
+                        <div class=" text-white text-base">
+                            جدول زمني سريع للمشاريع , بخطة تنفيذية وتنظيم دقيق للحصول على المخرجات بالجودة والسرعة
+                            المنشود
+                        </div>
+                    </div>
+                </div>
+
+                <div class=" border border-opacity-5 border-white  rounded-lg bg-white/10">
+                    <div class="flex flex-col gap-3 items-center">
+                        <div class="">
+                            <img src="/images/star.svg" alt="">
+                        </div>
+                        <div class="">
+                            <h1 class="text-white text-xl">سرعة التنفيذ</h1>
+                        </div>
+                        <div class=" text-white text-base">
+                            جدول زمني سريع للمشاريع , بخطة تنفيذية وتنظيم دقيق للحصول على المخرجات بالجودة والسرعة
+                            المنشود
+                        </div>
+                    </div>
+                </div>
+
+                <div class=" border  border-opacity-5 border-white rounded-lg bg-white/10">
+                    <div class="flex flex-col gap-3 items-center">
+                        <div class="">
+                            <img src="/images/star.svg" alt="">
+                        </div>
+                        <div class="">
+                            <h1 class="text-white text-xl">سرعة التنفيذ</h1>
+                        </div>
+                        <div class=" text-white text-base">
+                            جدول زمني سريع للمشاريع , بخطة تنفيذية وتنظيم دقيق للحصول على المخرجات بالجودة والسرعة
+                            المنشود
+                        </div>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </div>
+
     </div>
-    
-{{-- </div> --}}
+
+    {{-- </div> --}}
 </section>
-
-

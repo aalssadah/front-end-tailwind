ad0796d9c02ec036c77ce8f92a1dee44354e0d49
aalssadah <ala.alssadah@gmail.com>
2025-03-10 13:11:59 +0300

    center first choice

--- resources/views/components/home/first-choice.blade.php
+++ resources/views/components/home/first-choice.blade.php
@@ -1,9 +1,37 @@
-<section class="bg-cover bg-center" style="background-image: url('{{ asset('images/firstchoice.png') }}');">
+<section class="bg-cover bg-center pt-16  relative" style="background-image: url('{{ asset('images/firstchoice.png') }}');">
 
-    <div class=" ">
+    {{-- <div class=" max-w-6xl mx-auto "> --}}
+<div class=" absolute inset-0 backdrop-blur-sm"></div>
+        <div class=" flex flex-col gap-4 text-center">
+            
+        <div class=" texts flex flex-col gap-2" style="    transform: translateZ(0px);opacity: 1">
+            <div class="font-semibold text-xl " >
+                <h1 class=" text-white">مايجعلنا خيارك
+                    <span class=" text-teal-300 ">الأول</span>
+                </h1>
+            </div>
 
+            <div class=" mx-auto">
+                <img src="/images/dot.svg" class=" " alt="">
+            </div>
+            
+            <div class="">
+                <p class=" font-medium text-l text-white">
+                    نحن نقدم حلولاً إلكترونية مبتكرة تتكامل بين التسويق الفعّال وبرمجة المواقع والتطبيقات بأعلى جودة
+                    واهتمام بالتفاصيل
+                </p>
+            </div>
+            
+        </div>
 
+        <div class="boxes">
 
+        </div>
+        
+        
     </div>
-
+    
+{{-- </div> --}}
 </section>
+
+

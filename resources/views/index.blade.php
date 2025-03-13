481bcbdebee2e67d757bd43881a1ae7fbac3c726
aalssadah <ala.alssadah@gmail.com>
2025-03-13 12:41:00 +0300

    add section of stats

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,48 @@
 <x-app-layout>
 
-  <x-home.hero></x-home.hero>
+    <x-home.hero></x-home.hero>
 
-  <x-home.service></x-home.service>
+    <x-home.service></x-home.service>
 
-  <x-home.first-choice></x-home.first-choice>
+    <x-home.first-choice></x-home.first-choice>
+
+    <x-app></x-app>
+
+    <section class="stats bg-teal-100">
+
+        <div class=" max-w-4xl mx-auto flex flex-col md:flex-row px-8  py-24 justify-between ">
+
+            <div class="flex flex-col gap-1 text-center">
+                <div class="">
+                    <h1 class=" text-2xl font-semibold">عميل</h1>
+                </div>
+                <div class="">
+                    <p class=" font-medium opacity-30 ">يثقون بخدماتنا</p>
+                </div>
+                <div class=""></div>
+            </div>
+
+            <div class="flex flex-col gap-1  text-center">
+                <div class="">
+                    <h1 class=" text-2xl font-semibold">عميل</h1>
+                </div>
+                <div class="">
+                    <p class=" font-medium opacity-30 ">يثقون بخدماتنا</p>
+                </div>
+                <div class=""></div>
+            </div>
+            <div class="flex flex-col gap-1  text-center">
+                <div class="">
+                    <h1 class=" text-2xl font-semibold">عميل</h1>
+                </div>
+                <div class="">
+                    <p class=" font-medium opacity-30 ">يثقون بخدماتنا</p>
+                </div>
+                <div class=""></div>
+            </div>
+
+        </div>
+
+    </section>
 
 </x-app-layout>

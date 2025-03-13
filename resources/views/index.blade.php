64449696a857801f04d945334d75949336d2bdbd
aalssadah <ala.alssadah@gmail.com>
2025-03-13 14:10:43 +0300

    finish stats

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,38 +8,24 @@
 
     <x-app></x-app>
 
-    <section class="stats bg-teal-100/55">
+    <section class="stats bg-teal-100">
 
         <div class=" max-w-4xl mx-auto flex flex-col md:flex-row px-8  py-24 justify-between ">
 
-            <div class="flex flex-col gap-1 text-center">
-                <div class="">
-                    <h1 class=" text-2xl font-semibold">عميل</h1>
+            @for ($i = 0; $i < 3; $i++)
+                <div class="flex flex-col gap-3 text-center items-center">
+                    <div class=" w-16 h-16 bg-white rounded-2xl rotate-45 flex  justify-center items-center ">
+                        <h1 class="  text-teal-300 font-semibold text-2xl  -rotate-45">250</h1>
+                    </div>
+                    <div class="">
+                        <h1 class=" text-2xl font-semibold">عميل</h1>
+                    </div>
+                    <div class="">
+                        <p class=" font-medium opacity-30 ">يثقون بخدماتنا</p>
+                    </div>
+                    <div class=""></div>
                 </div>
-                <div class="">
-                    <p class=" font-medium opacity-30 ">يثقون بخدماتنا</p>
-                </div>
-                <div class=""></div>
-            </div>
-
-            <div class="flex flex-col gap-1  text-center">
-                <div class="">
-                    <h1 class=" text-2xl font-semibold">عميل</h1>
-                </div>
-                <div class="">
-                    <p class=" font-medium opacity-30 ">يثقون بخدماتنا</p>
-                </div>
-                <div class=""></div>
-            </div>
-            <div class="flex flex-col gap-1  text-center">
-                <div class="">
-                    <h1 class=" text-2xl font-semibold">عميل</h1>
-                </div>
-                <div class="">
-                    <p class=" font-medium opacity-30 ">يثقون بخدماتنا</p>
-                </div>
-                <div class=""></div>
-            </div>
+            @endfor
 
         </div>
 

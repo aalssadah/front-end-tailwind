9abd4452f51820fb0738d8a1701bdda30f2516f3
aalssadah <ala.alssadah@gmail.com>
2025-03-16 13:27:42 +0300

    edit contact us

--- resources/views/components/home/contact-us.blade.php
+++ resources/views/components/home/contact-us.blade.php
@@ -1,13 +1,22 @@
 <section class=" bg-[#E2F5F4] mt-16 " >   
-    <div class=" max-w-6xl m-auto py-12 mx-24 bg-red-200 grid grid-cols-1 md:grid-cols-4">
+    <div class=" max-w-full  md:max-w-6xl mx-auto  py-12 px-24 bg-red-200 grid grid-cols-1 md:grid-cols-4">
 
         <div class=" form col-span-3 bg-blue-200 py-12 mx-2 rounded-lg shadow-lg flex flex-col justify-between gap-4">
 
-            <div class="">
+            <div class="px-2">
                 <h1 class=" text-2xl ">نرحب بتواصلكم</h1>
                 <p class="text-base opacity-30 ">
                     مع نوايا خير أصبح النجاح والتميز اسهل الآن ما عليك سوى التواصل معنا ونحن نعرف جيدا كيف نساعدك
                 </p>
+                <div class="flex gap-3 flex-col md:flex-row ">
+                    <input type="text" placeholder="الإسم" class=" rounded-sm bg-gray-300 w-full">
+                    <input type="text" placeholder="الإيميل"  class=" rounded-sm bg-gray-300 w-full">
+                </div>
+                <div class=" mt-2">
+
+                    <input class="rounded-sm bg-gray-300 w-full" placeholder="العنوان"></textarea>
+                    <textarea class="rounded-sm bg-gray-300 w-full  mt-2" placeholder="الرسالة"></textarea>
+                </div>
             </div>
 
             <div class="">

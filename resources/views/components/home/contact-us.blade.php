8523071c076b41d9b6e4f873d505bc3d341066ef
aalssadah <ala.alssadah@gmail.com>
2025-03-16 13:01:58 +0300

    edit contact us

--- resources/views/components/home/contact-us.blade.php
+++ resources/views/components/home/contact-us.blade.php
@@ -1,3 +1,23 @@
-<div>
-   hello contact us
-</div>
+<section class=" bg-[#E2F5F4] mt-16 " >   
+    <div class=" max-w-6xl m-auto py-12 mx-24 bg-red-200 grid grid-cols-1 md:grid-cols-4">
+
+        <div class=" form col-span-3 bg-blue-200 py-12 mx-2 rounded-lg shadow-lg flex flex-col justify-between gap-4">
+
+            <div class="">
+                <h1 class=" text-2xl ">نرحب بتواصلكم</h1>
+                <p class="text-base opacity-30 ">
+                    مع نوايا خير أصبح النجاح والتميز اسهل الآن ما عليك سوى التواصل معنا ونحن نعرف جيدا كيف نساعدك
+                </p>
+            </div>
+
+            <div class="">
+                <a class=" bg-[#00c7c2] text-white rounded-full p-2">ارسل الان </a>
+            </div>
+        </div>
+
+        <div class=" address col-span-1 bg-green-300 py-12 mx-2 rounded-lg shadow-lg ">
+
+        </div>
+
+    </div>
+</section>

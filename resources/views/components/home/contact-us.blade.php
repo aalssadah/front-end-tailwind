8f66fdc6711dbf6993b08e2b0e976e825775acd2
aalssadah <ala.alssadah@gmail.com>
2025-03-18 14:34:23 +0300

    fix contact us

--- resources/views/components/home/contact-us.blade.php
+++ resources/views/components/home/contact-us.blade.php
@@ -1,25 +1,25 @@
-<section class=" bg-[#E2F5F4] mt-16 " >   
-    <div class=" max-w-full  md:max-w-6xl mx-auto  py-12 px-24 bg-red-200 grid grid-cols-1 md:grid-cols-4">
+<section class=" bg-[#E2F5F4] mt-16 ">
+    <div class=" max-w-full  md:max-w-6xl mx-auto  py-12 px-0 md:px-20  grid grid-cols-1 md:grid-cols-4">
 
-        <div class=" form col-span-3 bg-blue-200 py-12 mx-2 rounded-lg shadow-lg flex flex-col justify-between gap-4">
+        <div class=" form col-span-3  bg-white md:min-h-[400px] py-8  mx-2 rounded-2xl shadow-lg flex flex-col justify-between gap-4">
 
-            <div class="px-2">
+            <div class="px-4">
                 <h1 class=" text-2xl ">نرحب بتواصلكم</h1>
                 <p class="text-base opacity-30 ">
                     مع نوايا خير أصبح النجاح والتميز اسهل الآن ما عليك سوى التواصل معنا ونحن نعرف جيدا كيف نساعدك
                 </p>
                 <div class="flex gap-3 flex-col md:flex-row ">
-                    <input type="text" placeholder="الإسم" class=" rounded-sm bg-gray-300 w-full">
-                    <input type="text" placeholder="الإيميل"  class=" rounded-sm bg-gray-300 w-full">
+                    <input type="text" placeholder="الإسم" class=" rounded-sm bg-[#FAFAFA] w-full    p-2">
+                    <input type="text" placeholder="الإيميل" class=" rounded-sm bg-[#FAFAFA] w-full  p-2">
                 </div>
                 <div class=" mt-2">
 
-                    <input class="rounded-sm bg-gray-300 w-full" placeholder="العنوان"></textarea>
-                    <textarea class="rounded-sm bg-gray-300 w-full  mt-2" placeholder="الرسالة"></textarea>
+                    <input class="rounded-sm bg-[#FAFAFA] w-full p-2" placeholder="العنوان"></textarea>
+                    <textarea class="rounded-sm bg-[#FAFAFA] w-full p-2 mt-2" placeholder="الرسالة"></textarea>
                 </div>
             </div>
 
-            <div class="">
+            <div class="px-4">
                 <a class=" bg-[#00c7c2] text-white rounded-full p-2">ارسل الان </a>
             </div>
         </div>
